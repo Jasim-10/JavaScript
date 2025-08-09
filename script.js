d47d97f3e8e6bafc2589f36a0b51cc443b886dec
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-08-09 21:25:38 +0530

    JS

---
+++ script.js
@@ -0,0 +1,123 @@
+// console.log("HEllo World!");
+// console.log("Jasim Ansari");
+// let p = 10;
+// let q = 30;
+// console.log("sum is",p+q);
+// console.log("Hello ji");
+
+
+// let a = 10;
+// let b = 25;
+// console.log(`your sum is ${a+b}`);
+// console.log(`your sub is ${a-b}`);
+// console.log(`your mul is ${a*b}`);
+// console.log(`your div is ${a/b}`);
+
+
+
+// let age = 18;
+
+// if (age >= 18) {
+//     console.log("You are eligible to vote.");
+// } else {
+//     console.log("You are not eligible to vote.");
+// }
+
+
+
+// let marks = 85;
+
+// if (marks >= 90) {
+//     console.log("Grade: A+");
+// } else if (marks >= 75) {
+//     console.log("Grade: A");
+// } else if (marks >= 60) {
+//     console.log("Grade: B");
+// } else {
+//     console.log("Grade: C");
+// }
+
+
+// let day = 3;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+
+
+// let color = "red";
+// if (color == "red"){
+//     console.log("Stop")
+// }else if(color == "yellow"){
+//     console.log("slow down")
+// }else if(color ==  "green"){
+//     console.log("Go...")
+// }else {
+//     console.log("Invalid color")
+// }
+
+
+// let color = "green";
+// if (color == "red"){
+//     console.log("Stop")
+// }if(color == "yellow"){
+//     console.log("slow down")
+// }if(color ==  "green"){
+//     console.log("Go...")
+// }
+
+
+
+// let sizeOfPopcorn = "S";
+// if(sizeOfPopcorn == "XL"){
+//     console.log("price is rs.250")
+// }else if(sizeOfPopcorn == "L"){
+//     console.log("price is rs.200")
+// }else if(sizeOfPopcorn == "M"){
+//     console.log("price is rs.100")
+// }else {
+//     console.log("price is rs.50")
+// }
+
+
+
+// let string = "Asim"
+// if((string[0]=='a' || string[0] == 'A') && string.length>3){
+//     console.log("it is good string")
+// }else {
+//     console.log("not good string")
+// }
+
+
+// let num = 12;
+// if ((num%3==0)&&((num+1 == 15)|| (num-1 == 11))){
+//     console.log("safe")
+// }else {
+//     console.log('not safe')
+// }
+
+
+
