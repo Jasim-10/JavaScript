283a21dd669797a733a6d47be93a938a85611a5a
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-08-10 01:31:17 +0530

    JS

--- Class2/script.js
+++ Class2/script.js
@@ -1,7 +1,11 @@
+// trim():- removes whitespaces from both end of the string
 // let text = "                              Hello World!    ";
 // let newtext = text.trim();
 // console.log(text)
 // console.log(newtext);
+
+// let msg = "        jasim";
+// console.log(msg.trim());
 
 
 
