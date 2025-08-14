80f6c83282cf8ba2b49aad7aaa279f0ab6539a85
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-08-14 21:14:03 +0530

    JS

--- class 5/script.js
+++ class 5/script.js
@@ -50,9 +50,85 @@
 
 
 
-const student = {
-    name: "jasim",
-    age: 23,
-    marks:80,
-    city: "Patna"
-};
+// const student = {
+//     name: "jasim",
+//     age: 23,
+//     marks:80,
+//     city: "Patna"
+// };
+
+
+
+// const classInfo = {
+//     aman: {
+//         grade: "A+",
+//         city: "Delhi"
+//     },
+//     shradha: {
+//         grade: "A",
+//         city: "Mumbai"
+//     },
+//     karan: {
+//         grade: "O",
+//         city: "Pune"
+//     }
+// };
+// console.log(classInfo.aman.grade);
+// console.log(classInfo.aman.city);
+// console.log(classInfo.shradha.grade);
+// console.log(classInfo.shradha.city);
+// console.log(classInfo.karan.grade);
+// console.log(classInfo.karan.city);
+
+
+
+
+// const classInfo = [
+//     {
+//         name: "jasim",
+//         grade:"A"
+//     },
+//     {
+//         name: "asim",
+//         grade:"A+"
+//     },
+//     {
+//         name: "Nasim",
+//         grade:"O"
+//     }
+// ];
+// console.log(classInfo[0]);
+
+
+
+// // Math object
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.sqrt(25));
+// console.log(Math.E);
+// console.log(Math.random());
+// console.log(Math.abs(-25));
+// console.log(Math.round(42.14625));
+// console.log(Math.pow(2,5));
+// console.log(Math.floor(5.999999));
+// console.log(Math.ceil(5.4));
+// console.log(Math.ceil(5.5));
+
+
+
+// random integer between 1 to 10
+
+// let num = Math.random();
+// num = num*10;
+// // console.log(num)
+// num = Math.floor(num);
+// // console.log(num);
+// num = num+1;
+// console.log(num);
+
+// let random = Math.floor(Math.random() * 10)+1;
+// console.log(random);
+
+
+
+
