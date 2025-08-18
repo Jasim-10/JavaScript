3f49e1ed98da079e3fc2637c8de741b1dc05b810
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-08-18 15:40:28 +0530

    DOM

--- DOM/app.js
+++ DOM/app.js
@@ -1,12 +1,26 @@
 // getElementById is a method in JavaScript that is used to select an HTML element by its id attribute.
 // getElementsByClassName is a JavaScript method that selects all elements with the same class name
 
-let smallImages = document.getElementsByClassName("oldImg");
-for (let i=0;i<smallImages.length;i++){
-    console.dir(smallImages[i].src);
-}
+// let smallImages = document.getElementsByClassName("oldImg");
+// for (let i=0;i<smallImages.length;i++){
+//     console.dir(smallImages[i].src);
+// }
 
 
 
 // getElementsByTagName is a JavaScript method used to select all HTML elements with a given tag name (like p, div, h1, etc).
 
+// document.querySelector('p'); select first p element 
+// document.querySelector('#myId'); select first element with id = myId
+// document.querySelector('.myClass'); select first element with class = myClass
+// document.querySelectorALL('p'); select all p elements
+
+
+
+
+// console.dir(document.querySelector('h1'));
+// console.dir(document.querySelector('#description'));
+// console.dir(document.querySelector('.oldImg'));
+// console.dir(document.querySelectorAll('p'));
+// console.log(document.querySelector('h1').innerText);
+// console.dir(document.querySelectorAll("div a"));
