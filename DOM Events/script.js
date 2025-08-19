6db62a43332cd59d002daf02ce0e8e22b8169caf
Md Jasim Ansari <jasimcec2025@gmail.com>
2025-08-19 15:26:51 +0530

    DOM Events

---
+++ DOM Events/script.js
@@ -0,0 +1,44 @@
+// let btn = document.querySelector('button');
+// console.dir(btn);
+// // btn.onclick = function (){
+// //     console.log("button was click");
+// // };
+
+// let sayHello = (()=>{
+//     alert("hello");
+// });
+// btn.onclick = sayHello;
+
+
+// let btns = document.querySelectorAll("button");
+// for (btn of btns){
+//     btn.onclick = sayHello;
+// }
+// function sayHello(){
+//     console.log("button clicked");
+// };
+
+
+// let btns = document.querySelectorAll("button");
+// for (btn of btns) {
+//     btn.onmouseenter = sayHi;
+// }
+// function sayHi(){
+//     console.log("Mouse is enter");
+// }
+
+
+// let btns = document.querySelectorAll("button");
+// for (btn of btns) {
+//     btn.addEventListener("click",sayHello);
+//     btn.addEventListener("mouseenter",sayHi);
+// }
+// function sayHello(){
+//     console.log("button clicked");
+// };
+// function sayHi(){
+//     console.log("Mouse is enter");
+// }
+
+
+
